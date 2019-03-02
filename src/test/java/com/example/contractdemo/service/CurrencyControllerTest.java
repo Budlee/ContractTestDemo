17342f915eabe358a79e71aa0f1c4168d7f85574
MattyA <2326918+Budlee@users.noreply.github.com>
2019-03-02 13:14:24 +0000

    Completed adding additional tests to new folder

---
+++ src/test/java/com/example/contractdemo/service/CurrencyControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.contractdemo.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrencyControllerTest {
+
+
+    @Test
+    public void cheecky_test(){
+        var cc = new CurrencyController();
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("abcdefg");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        assertEquals(3,cc.getSupportedCurrencyValues().size());
+    }
+}
